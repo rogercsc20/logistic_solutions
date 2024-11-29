f1fde42e0584cf33b2016c0efbde5da8cf3a2470
Alex <rogercsc20@gmail.com>
2024-11-28 18:35:07 -0600

    si ya alv

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,16 @@
-import os
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("app.config.Config")
 
     db.init_app(app)
-    
+    migrate.init_app(app, db)
+
     return app
